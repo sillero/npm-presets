59bdb2b62d568d220dfa540d7a4f9eca0cc729d9
Gustavo Sillero <gustavo.sillero@iwsbrazil.com>
2016-05-15 03:11:09 -0300

    update react-redux-webpack

--- presets/react-redux-webpack.js
+++ presets/react-redux-webpack.js
@@ -6,6 +6,9 @@ module.exports = [
   'react-router',
   'react-router-redux',
   'redux',
+  'redux-devtools',
+  'redux-devtools-dock-monitor',
+  'redux-devtools-log-monitor',
   'redux-saga',
   'webpack',
   'webpack-dev-server'
